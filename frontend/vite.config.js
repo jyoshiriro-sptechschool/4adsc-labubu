dbb1511c2854d82285bcf9314be68949bf441e48
José Yoshiriro (SpTech) <jose.yoshihiro@bandtec.com.br>
2025-11-26 20:29:38 -0300

    Add files via upload

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+})
